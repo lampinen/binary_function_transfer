8e628b6ec189d531d7d17981801d64c6ccd37500
Andrew Lampinen <lampinen@stanford.edu>
2019-04-23 12:58:49 -0700

    Setting up for analyses of generalization to held out examples

--- running_transfer_nonlinear.py
+++ running_transfer_nonlinear.py
@@ -11,18 +11,18 @@ PI = np.pi
 num_input_per = 5
 num_hidden = 40
 num_runs = 50 
+num_test = 4 # how many datapoints to hold out for eval 
 learning_rate = 0.005
 num_epochs = 20000
 num_layers = 4
 init_mult = 1.
-output_dir = "results_second_train_both_nh_%i_lr_%.4f_im_%.2f/" %(num_hidden, learning_rate, init_mult)
+output_dir = "results_generalization_nh_%i_lr_%.4f_im_%.2f/" %(num_hidden, learning_rate, init_mult)
 save_every = 5
 tf_pm = True # if true, code t/f as +/- 1 rather than 1/0
 train_sequentially = True # If true, train task 2 and then task 1
-second_train_both = True # If train_sequentially, whether to continue training on task 2 while training task 1
+second_train_both = False # If train_sequentially, whether to continue training on task 2 while training task 1
 batch_size = 4
 early_stopping_thresh = 0.005
-
 ###
 if not os.path.exists(os.path.dirname(output_dir)):
     os.makedirs(os.path.dirname(output_dir))
@@ -76,107 +76,113 @@ for input_shared in [False]:#, True]:
                 elif t2 == "XAO":
                     x2_data, y2_data = datasets.XAO_dataset(num_input_per)
 
-                if t2 == "None":
-                    if input_shared:
-                        x_data = x1_data
-                    else:
-                        x_data = np.concatenate([x1_data, np.zeros_like(x1_data)], axis=1)
-                    y_data = np.concatenate([y1_data, np.zeros_like(y1_data)], axis=1)
-                else:
-                    if input_shared:
-                        x_data = np.concatenate([x1_data, x2_data], axis=0)
-                    else:
-                        x_data = np.concatenate([np.concatenate([x1_data, np.zeros_like(x2_data)], axis=1),
-                                                 np.concatenate([np.zeros_like(x1_data), x2_data], axis=1)], axis=0)
-                    y_data = np.concatenate([np.concatenate([y1_data, np.zeros_like(y2_data)], axis=1),
-                                             np.concatenate([np.zeros_like(y1_data), y2_data], axis=1)], axis=0)
-
                 if tf_pm:
-                    x_data = 2*x_data - 1
-                    y_data = 2*y_data - 1
+                    x1_data = 2*x1_data - 1
+                    y1_data = 2*y1_data - 1
+		    if t2 != "None":
+			x2_data = 2*x2_data - 1
+			y2_data = 2*y2_data - 1
 
                 output_size = 1
     
-                num_datapoints = len(x_data)
-                batch_subset = len(x1_data)
-                
-                if input_shared:
-                    N1 = num_input_per
-                else:
-                    N1 = 2*num_input_per
-                    
-                input_ph = tf.placeholder(tf.float32, shape=[None, N1])
-                target_ph = tf.placeholder(tf.float32, shape=[None, 2*output_size])
+                num_datapoints = len(x1_data)
+		num_train = num_datapoints - num_test
 
-                W = tf.get_variable('Wi', shape=[N1, num_hidden], initializer=var_scale_init)
-                b = tf.get_variable('Bi', shape=[num_hidden,], initializer=tf.zeros_initializer)
-                hidden = nonlinearity(tf.matmul(input_ph, W) + b)
+		order1 = np.random.permutation(num_datapoints)
+		order2 = np.random.permutation(num_datapoints)
+		x1_test_data = x1_data[order1[-num_test:]]
+		y1_test_data = y1_data[order1[-num_test:]]
+		x1_data = x1_data[order1[:num_train]]
+		y1_data = y1_data[order1[:num_train]]
+
+		if t2 != "None":
+		    x2_test_data = x2_data[order2[-num_test:]]
+		    y2_test_data = y2_data[order2[-num_test:]]
+		    x2_data = x2_data[order2[:num_train]]
+		    y2_data = y2_data[order2[:num_train]]
+
+                input_1_ph = tf.placeholder(tf.float32, shape=[None, num_input_per])
+                input_2_ph = tf.placeholder(tf.float32, shape=[None, num_input_per])
+                target_ph = tf.placeholder(tf.float32, shape=[None, output_size])
+
+                W1 = tf.get_variable('Widom1', shape=[num_input_per, num_hidden], initializer=var_scale_init)
+                b1 = tf.get_variable('Bidom1', shape=[num_hidden,], initializer=tf.zeros_initializer)
+                W2 = tf.get_variable('Widom2', shape=[num_input_per, num_hidden], initializer=var_scale_init)
+                b2 = tf.get_variable('Bidom2', shape=[num_hidden,], initializer=tf.zeros_initializer)
+                hidden1 = nonlinearity(tf.matmul(input_1_ph, W1) + b1)
+                hidden2 = nonlinearity(tf.matmul(input_2_ph, W2) + b2)
 
                 for layer_i in range(1, num_layers-1):
                     W = tf.get_variable('Wh%i' % layer_i, shape=[num_hidden, num_hidden], initializer=var_scale_init)
                     b = tf.get_variable('B%i' % layer_i, shape=[num_hidden,], initializer=tf.zeros_initializer)
-                    hidden = nonlinearity(tf.matmul(hidden, W) + b)
+                    hidden1 = nonlinearity(tf.matmul(hidden1, W) + b)
+                    hidden2 = nonlinearity(tf.matmul(hidden2, W) + b)
 
-                W = tf.get_variable('Wo', shape=[num_hidden, 2*output_size], initializer=var_scale_init)
-                b = tf.get_variable('Bo', shape=[2*output_size,], initializer=tf.zeros_initializer)
-                output = nonlinearity(tf.matmul(hidden, W) + b)
-                
+                W1 = tf.get_variable('Wodom1', shape=[num_hidden, output_size], initializer=var_scale_init)
+                b1 = tf.get_variable('Bodom1', shape=[output_size,], initializer=tf.zeros_initializer)
+                W2 = tf.get_variable('Wodom2', shape=[num_hidden, output_size], initializer=var_scale_init)
+                b2 = tf.get_variable('Bodom2', shape=[output_size,], initializer=tf.zeros_initializer)
+                output1 = nonlinearity(tf.matmul(hidden1, W1) + b1)
+                output2 = nonlinearity(tf.matmul(hidden2, W2) + b2)
 
-                domain_one_mask = np.concatenate([np.ones(batch_subset, dtype=np.bool), np.zeros(batch_subset, dtype=np.bool)], axis=0)
-                d1_mask_ph = tf.placeholder(tf.bool, shape=[None,])
-                first_domain_loss = tf.nn.l2_loss(tf.boolean_mask(output[:, :output_size] - target_ph[:, :output_size], d1_mask_ph))
-                second_domain_loss = tf.nn.l2_loss(tf.boolean_mask(output[:, output_size:] - target_ph[:, output_size:], tf.logical_not(d1_mask_ph)))
-
-                if t2 == "None":
-                    loss = first_domain_loss
-                else:
-                    loss = first_domain_loss + second_domain_loss
+                first_domain_loss = tf.nn.l2_loss(output1 - target_ph)
+                second_domain_loss = tf.nn.l2_loss(output2 - target_ph)
+                    
                 optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-                train = optimizer.minimize(loss)	
                 fd_train = optimizer.minimize(first_domain_loss)
                 sd_train = optimizer.minimize(second_domain_loss)
             
                 with tf.Session() as sess:
-                    def train_epoch():
-                        this_order = np.random.permutation(num_datapoints)
-                        for batch_i in xrange(num_datapoints//batch_size):
-                            sess.run(train, feed_dict={input_ph: x_data[this_order[batch_i*batch_size:(batch_i+1)*batch_size], :], target_ph: y_data[this_order[batch_i*batch_size:(batch_i+1)*batch_size], :], d1_mask_ph: domain_one_mask[this_order[batch_i*batch_size:(batch_i+1)*batch_size]]})
-
                     def train_epoch_1():
-                        this_order = np.random.permutation(num_datapoints)
-                        for batch_i in xrange(num_datapoints//batch_size):
-                            sess.run(fd_train, feed_dict={input_ph: x_data[this_order[batch_i*batch_size:(batch_i+1)*batch_size], :], target_ph: y_data[this_order[batch_i*batch_size:(batch_i+1)*batch_size], :], d1_mask_ph: domain_one_mask[this_order[batch_i*batch_size:(batch_i+1)*batch_size]]})
+                        this_order = np.random.permutation(num_train)
+                        for batch_i in xrange(num_train//batch_size):
+                            feed_dict = {input_1_ph: x1_data[this_order[batch_i*batch_size:(batch_i+1)*batch_size], :], 
+                            		 target_ph: y1_data[this_order[batch_i*batch_size:(batch_i+1)*batch_size], :]} 
+                            sess.run(fd_train, feed_dict=feed_dict)
 
                     def train_epoch_2():
-                        this_order = np.random.permutation(num_datapoints)
-                        for batch_i in xrange(num_datapoints//batch_size):
-                            sess.run(sd_train, feed_dict={input_ph: x_data[this_order[batch_i*batch_size:(batch_i+1)*batch_size], :], target_ph: y_data[this_order[batch_i*batch_size:(batch_i+1)*batch_size], :], d1_mask_ph: domain_one_mask[this_order[batch_i*batch_size:(batch_i+1)*batch_size]]})
+                        this_order = np.random.permutation(num_train)
+                        for batch_i in xrange(num_train//batch_size):
+                            feed_dict = {input_2_ph: x2_data[this_order[batch_i*batch_size:(batch_i+1)*batch_size], :], 
+                                         target_ph: y2_data[this_order[batch_i*batch_size:(batch_i+1)*batch_size], :]}
+                            sess.run(sd_train, feed_dict=feed_dict)
 
 
                     def evaluate():
                         curr_loss1 = 0.
                         curr_loss2 = 0.
-                        for batch_i in xrange(num_datapoints//batch_size):
-                            curr_loss1 += sess.run(first_domain_loss, feed_dict={input_ph: x_data[batch_i*batch_size:(batch_i+1)*batch_size, :], target_ph: y_data[batch_i*batch_size:(batch_i+1)*batch_size, :], d1_mask_ph: domain_one_mask[batch_i*batch_size:(batch_i+1)*batch_size]})
-                            curr_loss2 += sess.run(second_domain_loss, feed_dict={input_ph: x_data[batch_i*batch_size:(batch_i+1)*batch_size, :], target_ph: y_data[batch_i*batch_size:(batch_i+1)*batch_size, :], d1_mask_ph: domain_one_mask[batch_i*batch_size:(batch_i+1)*batch_size]})
-                        return curr_loss1/batch_subset, curr_loss2/batch_subset
+                        for batch_i in xrange(num_train//batch_size):
+			    curr_loss1 += sess.run(first_domain_loss, feed_dict={input_1_ph: x1_data[batch_i*batch_size:(batch_i+1)*batch_size, :], target_ph: y1_data[batch_i*batch_size:(batch_i+1)*batch_size, :]})
+			if t2 != "None":
+			    for batch_i in xrange(num_train//batch_size):
+				curr_loss2 += sess.run(second_domain_loss, feed_dict={input_2_ph: x2_data[batch_i*batch_size:(batch_i+1)*batch_size, :], target_ph: y2_data[batch_i*batch_size:(batch_i+1)*batch_size, :]})
+                        curr_test_loss1 = 0.
+                        curr_test_loss2 = 0.
+                        for batch_i in xrange(num_test//batch_size):
+			    curr_test_loss1 += sess.run(first_domain_loss, feed_dict={input_1_ph: x1_test_data[batch_i*batch_size:(batch_i+1)*batch_size, :], target_ph: y1_test_data[batch_i*batch_size:(batch_i+1)*batch_size, :]})
+			if t2 != "None":
+			    for batch_i in xrange(num_test//batch_size):
+				curr_test_loss2 += sess.run(second_domain_loss, feed_dict={input_2_ph: x2_test_data[batch_i*batch_size:(batch_i+1)*batch_size, :], target_ph: y2_test_data[batch_i*batch_size:(batch_i+1)*batch_size, :]})
+                        return curr_loss1/num_train, curr_test_loss1/num_test, curr_loss2/num_train, curr_test_loss2/num_test
                     
                     sess.run(tf.global_variables_initializer())
                         
                     with open("%s%s.csv" % (output_dir, filename_prefix), "w") as fout:
-                        fout.write("epoch, loss1, loss2\n")
-                        loss1, loss2 = evaluate()
-                        print("%i, %f, %f\n" % (0, loss1, loss2))
-                        fout.write("%i, %f, %f\n" % (0, loss1, loss2))
+                        fout.write("epoch, loss1, loss1_test, loss2, loss2_test\n")
+                        losses = evaluate()
+			this_result = "%i, %f, %f, %f, %f\n" % tuple([0] + [l for l in losses])
+                        print(this_result)
+                        fout.write(this_result)
                         if train_sequentially:
                             if t2 != "None":
                                 for epoch_i in xrange(1, num_epochs + 1):
                                     train_epoch_2()	
                                     if epoch_i % save_every == 0:
-                                        loss1, loss2 = evaluate()
-                                        print("%i, %f, %f\n" % (epoch_i, loss1, loss2))
-                                        fout.write("%i, %f, %f\n" % (epoch_i, loss1, loss2))
-                                        if loss2 < early_stopping_thresh:
+					losses = evaluate()
+					this_result = "%i, %f, %f, %f, %f\n" % tuple([epoch_i] + [l for l in losses])
+					print(this_result)
+					fout.write(this_result)
+                                        if losses[2] < early_stopping_thresh:
                                             print("Early stop prior!")
                                             break
                             for epoch_i in xrange(num_epochs+1, 2*num_epochs + 1):
@@ -185,10 +191,11 @@ for input_shared in [False]:#, True]:
                                 else: 
                                     train_epoch_1()
                                 if epoch_i % save_every == 0:
-                                    loss1, loss2 = evaluate()
-                                    print("%i, %f, %f\n" % (epoch_i, loss1, loss2))
-                                    fout.write("%i, %f, %f\n" % (epoch_i, loss1, loss2))
-                                    if loss1 < early_stopping_thresh:
+				    losses = evaluate()
+				    this_result = "%i, %f, %f, %f, %f\n" % tuple([epoch_i] + [l for l in losses])
+				    print(this_result)
+				    fout.write(this_result)
+				    if losses[0] < early_stopping_thresh:
                                         print("Early stop main!")
                                         break
 
@@ -196,10 +203,11 @@ for input_shared in [False]:#, True]:
                             for epoch_i in xrange(1, num_epochs + 1):
                                 train_epoch()	
                                 if epoch_i % save_every == 0:
-                                    loss1, loss2 = evaluate()
-                                    print("%i, %f, %f\n" % (epoch_i, loss1, loss2))
-                                    fout.write("%i, %f, %f\n" % (epoch_i, loss1, loss2))
-                                    if loss1 < early_stopping_thresh and loss2 < early_stopping_thresh:
+				    losses = evaluate()
+				    this_result = "%i, %f, %f, %f, %f\n" % tuple([epoch_i] + [l for l in losses])
+				    print(this_result)
+				    fout.write(this_result)
+				    if losses[0] < early_stopping_thresh:
                                         print("Early stop!")
                                         break
 
